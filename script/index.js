3ddbca5a258072e41c70da8689555839982cb912
Gustavo Zamai <gustavosimaozamai@gmail.com>
2025-01-08 11:23:34 -0300

    Add a function to pick a winner, separete responsibilities

--- script/index.js
+++ script/index.js
@@ -1,11 +1,11 @@
 function changeDiceImage () {
     const diceOne = document.querySelector('#dice1');
-    const firstRandomNumber = Math.floor(Math.random() * 6) + 1;
+    var firstNumber = firstRandomNumber = Math.floor(Math.random() * 6) + 1;
 
     diceOne.src = `/assets/images/dice-${firstRandomNumber}.svg`;
 
     const diceTwo = document.querySelector('#dice2');
-    const secondRandomNumber = Math.floor(Math.random() * 6) + 1;
+    var secondNumber = secondRandomNumber = Math.floor(Math.random() * 6) + 1;
 
     diceTwo.src = `/assets/images/dice-${secondRandomNumber}.svg`;
 
@@ -18,10 +18,21 @@ function changeDiceImage () {
     }
 }
 
+function pickAWinner (firstNumber, secondNumber) {
+    if (firstNumber > secondNumber) {
+        document.querySelector('h1').textContent = 'Player 1 wins!';
+    } else if (firstNumber < secondNumber) {
+        document.querySelector('h1').textContent = 'Player 2 wins!';
+    } else {
+        document.querySelector('h1').textContent = 'It\'s a tie!';
+    }
+}
+
 var btn = document.getElementById('roll-dice');
 
 
 btn.addEventListener('click', function() {
     changeDiceImage();
+    pickAWinner(firstNumber, secondNumber);
 });
 
